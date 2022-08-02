603aadfadc38f16a03babc30e4a054ab06ff02b0
Rodrigo <rlopezdominguez19@gmail.com>
2022-08-02 15:29:05 -0300

    Se corrigió error de duplicidad de rta al form

--- js/app.js
+++ js/app.js
@@ -34,6 +34,10 @@ let rewardAutolykos = 31594;
 const button = document.getElementById('btnCalcular');
 button.addEventListener('click', (e) => {
     e.preventDefault() // ! El formulaio no recarga / no se va para arriba
+    
+/*     // * Eliminamos div-answer para no duplicar respuestas
+    $(".div-answer").remove(); */
+
     let extract = "";
     let seleccion = parseInt(eAlgo.value);
     let mhsUsuario = eMhs.value;
@@ -54,19 +58,21 @@ button.addEventListener('click', (e) => {
     } else if (seleccion === 2){
         extract = `Obtendrás ${rvnReward.toFixed(2)} RVN diario con ${mhsUsuario} MH/s`;
     } else if (seleccion === 3) {
-            extract = `Obtendrás ${ergoReward.toFixed(2)} ERG diario con ${mhsUsuario} MH/s`;
+        extract = `Obtendrás ${ergoReward.toFixed(2)} ERG diario con ${mhsUsuario} MH/s`;
     } else {
-            extract = `Debe seleccionar un algoritmo válido e ingresar su poder de cómputo`;
+        extract = `Debe seleccionar un algoritmo válido e ingresar su poder de cómputo`;
     }
-        
-    let ul = document.getElementsByClassName("main-form")[0];
-    let div = document.createElement("div");
-    div.setAttribute("class", "div-answer");
-        
-    div.innerHTML = `
-    <img src="" alt="" id="img-answer">
-    <p>${extract}</p>`;
-    ul.appendChild(div);
+
+    let divAnswer = document.getElementById("divAnswer");
+
+    function addContentDiv() {
+        let div = divAnswer;
+        div.innerHTML = `
+        <img src="" alt="" id="img-answer">
+        <p>${extract}</p>`;
+    }
+
+    addContentDiv();
 });
 
 // ! Lógica para el funcionamiento del pop-up
